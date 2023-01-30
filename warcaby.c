9de42fea94bb7cf36c5ac048e43488221741fed5
Kamil <kpurgat.zsp9@gmail.com>
2023-01-30 11:31:39 -0800

    Added white and black pawns as dynamically allocated structure objects

--- warcaby.c
+++ warcaby.c
@@ -14,9 +14,21 @@
 struct WhitePawn{
     int xPosition;
     int yPosition;
-    int size;
+    int outerSize;
+    int innerSize;
     int isKing;
-    enum color c;
+    enum color outerC;
+    enum color innerC;
+  };
+
+  struct BlackPawn{
+    int xPosition;
+    int yPosition;
+    int outerSize;
+    int innerSize;
+    int isKing;
+    enum color outerC;
+    enum color innerC;
   };
 
 void makeCheckersBackground(void){
@@ -27,7 +39,7 @@ void makeCheckersBackground(void){
         gfx_filledRect(BEGIN_OF_BOARD_W + (i * FIELD_SIZE), BEGIN_OF_BOARD_H + (j * FIELD_SIZE), BEGIN_OF_BOARD_W + (i * FIELD_SIZE) + FIELD_SIZE, BEGIN_OF_BOARD_H + (j * FIELD_SIZE) + FIELD_SIZE, LIGHT_YELLOW);
     }
   }
-  gfx_updateScreen();
+  //gfx_updateScreen();
 }
 
 void makePawns(void){
@@ -40,34 +52,86 @@ int main() {
     exit(3);
   makeCheckersBackground();
 
-  struct WhitePawn *WP = malloc(15 * sizeof(*WP));
+  struct WhitePawn *WP = malloc(15 * sizeof(*WP));  //memory allocation for white pawns
 
   int counter = 0;
 
+  //Creating structs objects that are representing white pawns
   for(int i = 0; i <= 4; ++i, ++counter){
-    WP[counter].xPosition = 2 * i;
+    WP[counter].xPosition = ((2 * i) + 1);
     WP[counter].yPosition = 0;
-    WP[counter].size = 15;
+    WP[counter].outerSize = 23;
+    WP[counter].innerSize = 20;
     WP[counter].isKing = 0;
-    WP[counter].c = WHITE;
-  }
-
-  for(int i = 0; i <= 4; ++i, ++counter){
-    WP[counter].xPosition = (2 * i) + 1;
-    WP[counter].yPosition = 1;
-    WP[counter].size = 15;
-    WP[counter].isKing = 0;
-    WP[counter].c = WHITE;
+    WP[counter].outerC = BLACK;
+    WP[counter].innerC = WHITE;
   }
 
   for(int i = 0; i <= 4; ++i, ++counter){
     WP[counter].xPosition = 2 * i;
-    WP[counter].yPosition = 2;
-    WP[counter].size = 15;
+    WP[counter].yPosition = 1;
+    WP[counter].outerSize = 23;
+    WP[counter].innerSize = 20;
     WP[counter].isKing = 0;
-    WP[counter].c = WHITE;
+    WP[counter].outerC = BLACK;
+    WP[counter].innerC = WHITE;
   }
-  
+
+  for(int i = 0; i <= 4; ++i, ++counter){
+    WP[counter].xPosition = ((2 * i) + 1);
+    WP[counter].yPosition = 2;
+    WP[counter].outerSize = 23;
+    WP[counter].innerSize = 20;
+    WP[counter].isKing = 0;
+    WP[counter].outerC = BLACK;
+    WP[counter].innerC = WHITE;
+  }
+
+  struct BlackPawn *BP = malloc(15 * sizeof(*BP));  //memory allocation for black pawns
+
+  counter = 0;
+
+  //Creating structs objects that are representing black pawns
+  for(int i = 0; i <= 4; ++i, ++counter){
+    BP[counter].xPosition = 9 - ((2 * i) + 1);
+    BP[counter].yPosition = 9;
+    BP[counter].outerSize = 23;
+    BP[counter].innerSize = 20;
+    BP[counter].isKing = 0;
+    BP[counter].outerC = BLACK;
+    BP[counter].innerC = BLACK;
+  }
+
+  for(int i = 0; i <= 4; ++i, ++counter){
+    BP[counter].xPosition = 9 - (2 * i);
+    BP[counter].yPosition = 8;
+    BP[counter].outerSize = 23;
+    BP[counter].innerSize = 20;
+    BP[counter].isKing = 0;
+    BP[counter].outerC = BLACK;
+    BP[counter].innerC = BLACK;
+  }
+
+  for(int i = 0; i <= 4; ++i, ++counter){
+    BP[counter].xPosition = 9 - ((2 * i) + 1);
+    BP[counter].yPosition = 7;
+    BP[counter].outerSize = 23;
+    BP[counter].innerSize = 20;
+    BP[counter].isKing = 0;
+    BP[counter].outerC = BLACK;
+    BP[counter].innerC = BLACK;
+  }
+
+  //Drawing pawns in starting positions
+  for(int i = 0; i < 15; ++i){
+    gfx_filledCircle((BEGIN_OF_BOARD_W + (FIELD_SIZE / 2) + (FIELD_SIZE * WP[i].xPosition)), (BEGIN_OF_BOARD_H + (FIELD_SIZE / 2) + (FIELD_SIZE * WP[i].yPosition)), WP[i].outerSize, WP[i].outerC);
+    gfx_filledCircle((BEGIN_OF_BOARD_W + (FIELD_SIZE / 2) + (FIELD_SIZE * WP[i].xPosition)), (BEGIN_OF_BOARD_H + (FIELD_SIZE / 2) + (FIELD_SIZE * WP[i].yPosition)), WP[i].innerSize, WP[i].innerC);
+  }
+  for(int i = 0; i < 15; ++i){
+    gfx_filledCircle((BEGIN_OF_BOARD_W + (FIELD_SIZE / 2) + (FIELD_SIZE * BP[i].xPosition)), (BEGIN_OF_BOARD_H + (FIELD_SIZE / 2) + (FIELD_SIZE * BP[i].yPosition)), BP[i].outerSize, BP[i].outerC);
+    gfx_filledCircle((BEGIN_OF_BOARD_W + (FIELD_SIZE / 2) + (FIELD_SIZE * BP[i].xPosition)), (BEGIN_OF_BOARD_H + (FIELD_SIZE / 2) + (FIELD_SIZE * BP[i].yPosition)), BP[i].innerSize, BP[i].innerC);
+  }
+  gfx_updateScreen();
   /*for(int i = 0; i < 15; ++i){
     printf("x:%d y:%d\n", WP[i].xPosition, WP[i].yPosition);
   }*/
